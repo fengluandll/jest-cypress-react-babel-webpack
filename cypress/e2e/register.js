5465b4ae5c97731dcc46c09caf8942549c1c3b85
wpannell <wil.pannell@gmail.com>
2020-01-05 01:05:09 -0800

    let the test submit a user for registration

--- cypress/e2e/register.js
+++ cypress/e2e/register.js
@@ -4,6 +4,11 @@ describe('register', () => {
   it('registers a new user', () => {
     const user = buildUser()
     
+    // noinspection JSCheckFunctionSignatures
     cy.visit('/')
+      .findByText(/register/i).click()
+      .findByLabelText(/username/i).type(user.username)
+      .findByLabelText(/password/i).type(user.password)
+      .findByText(/submit/i).click()
   })
 });
